0b6b355a195f01ebf49474770004187da1b3cc32
Tikesh Dewangan <dewangantikesh57@gmail.com>
2026-03-30 18:35:54 +0530

    Added requirements and Procfile for real

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-fh(3m$1!-qcxiw-@^jdq-sdms^v8(+6jx^e_ukx1$3^bvqg2f6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']  # Ya phir '.onrender.com' daal sakte ho
 
 # Application definition
 
@@ -48,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -118,4 +118,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
